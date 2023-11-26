466f56bf78062d823f7431a61e275eee48047a88
Ezequiel Cardoso <ezequielcardosno@gmail.com>
2023-11-26 01:23:10 -0300

    Envio da classe Curso

---
+++ Curso.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Classe feita por Ezequiel
+public class Curso {
+    private String nome;
+    private Professor professor;
+    private List<Aluno> alunos;
+    private List<Grupo> grupos;
+
+    public Curso(String nome, Professor professor) {
+        this.nome = nome;
+        this.professor = professor;
+        this.alunos = new ArrayList<>();
+        this.grupos = new ArrayList<>();
+    }
+
+    public void listarAlunos() {
+        System.out.println("Alunos no curso de " + nome + ":");
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno.getNome());
+        }
+    }
+
+    public void addGrupo(Grupo grupo) {
+        grupos.add(grupo);
+    }
+
+    // Insira seu método aqui abaixo
+
+    // Insira seu método aqui acima
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public List<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso [Nome: " + nome + ", Professor: " + professor + ", Alunos:" + alunos + "]";
+    }
+}
